a8afcaf2620e8cc35bed24b4c283af35993ad21e
avi <avimaybe7@gmail.com>
2025-10-08 20:08:06 +0530

    trying to fix

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -12,6 +13,31 @@ const nextConfig: NextConfig = {
         hostname: "placehold.co",
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [
+        ({ request }, callback) => {
+          if (
+            [
+              "@cloudflare/next-on-pages/next-image-loader",
+              "next",
+              "react",
+              "react-dom",
+            ].includes(request) ||
+            /^(next|react|react-dom|@supabase|@dnd-kit|framer-motion)/.test(request)
+          ) {
+            return callback();
+          }
+          if (!path.isAbsolute(request) && !request.startsWith(".")) {
+            return callback(null, `commonjs ${request}`);
+          }
+          callback();
+        },
+        ...config.externals,
+      ];
+    }
+    return config;
   },
 };
 
